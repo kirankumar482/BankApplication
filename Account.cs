089b44c175a2f2836e072e020e75d58dcc00ae36
kirankumar482 <kiran.valuecreedintern2024@gmail.com>
2024-08-08 17:09:23 +0500

    Added Project files

---
+++ Account.cs
@@ -0,0 +1,27 @@
+﻿namespace bankapp;
+
+public class Account
+{
+	private string id;
+	private string name;
+	private int balance;
+
+	public Account(string id,string name,int balance)
+	{
+		this.id = id;
+		this.name = name;
+		this.balance = balance;
+	}
+
+    //withdraw 
+
+
+    //deposit
+
+
+    //balance
+
+
+    //getters and setters
+
+}
---
+++ BankApplication.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace bankapp;
+
+public class BankApplication
+{
+    static void Main()
+    {
+        Console.WriteLine("Let's Build Bank Application");
+        Account acc = new Account("kkr","kiran",567);
+        acc.ToString();
+        //List of Accounts
+        //switch statements (create,access)
+        //if create create account obj and append (take details)
+        //else if access account using id of acc (deposit,withdraw)
+    }
+}
